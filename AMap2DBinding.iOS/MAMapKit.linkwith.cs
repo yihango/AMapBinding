8f0fccd56b220fa54b3e69e72ef9324c154dffbf
stan e <yi.hang@live.com>
2018-03-18 13:27:06 +0800

    修改本地库引用

---
+++ AMap2DBinding.iOS/MAMapKit.linkwith.cs
@@ -0,0 +1,8 @@
+using System;
+using ObjCRuntime;
+
+[assembly: LinkWith("MAMapKit.a",
+    LinkTarget.ArmV7 | LinkTarget.Simulator,
+    Frameworks = "GLKit OpenGLES UIKit Foundation CoreGraphics QuartzCore CoreLocation CoreTelephony SystemConfiguration Security AdSupport JavaScriptCore",
+    LinkerFlags = "-lz -lstdc++ -lc++",
+    ForceLoad = true)]
